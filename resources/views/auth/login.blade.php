aa47da43b65d33c9f1f7e04697d30802642ed7a1
miang <miguelamadormj@gmail.com>
2023-02-15 16:50:15 -0500

    Update: login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,5 +43,8 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+            {{ __('Do not have a account?') }}
+        </a>
     </form>
 </x-guest-layout>
